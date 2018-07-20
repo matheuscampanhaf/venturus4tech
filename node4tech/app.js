de44bcad1625695ba5a6dc42df5de78e7baa1e54
Matheus Campanha <matheuscampanhaf@gmail.com>
2018-07-20 13:00:52 -0300

    commits do terceiro dia

--- node4tech/app.js
+++ node4tech/app.js
@@ -10,48 +10,68 @@ const bodyParser = require('body-parser');
 const createVaga = (obj) => new Vaga(obj.id, obj.name, obj.description,
     obj.skills, obj.salary, obj.area, obj.differentials, obj.isPcd, obj.isActive);
 
-app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 app.use(bodyParser.json());
 // Async: evita que fica esperando
-app.get('/', async (requestAnimationFrame,res) => {
+app.get('/', async (requestAnimationFrame, res) => {
     try {
-     res.send('Hello World');
+        res.send('Hello World');
     } catch (error) {
 
     }
 })
 
-app.get('/vagas', async (req,res) => {
+app.get('/vagas', async (req, res) => {
     try {
         res.send(vagas);
-    } catch (error){
+    } catch (error) {
 
     }
 })
 
-app.get('/vagas/:id_vaga', async (req,res) =>{
-    try{
-        console.log('test');
+app.get('/vagas/:id_vaga', async (req, res) => {
+    try {
         let id_param = req.params.id_vaga;
-        result = (vagas.find(function (element){
-            return element.id = id_param;
+        result = (vagas.find(function (element) {
+            return element.id == id_param;
         }));
-        if (result === null) return res.status(500).send('Internal error');
+        if (result == null) return res.status(500).send('Internal error');
         return res.send(result);
-    } catch (error){
+    } catch (error) {
         return res.status(500).send('Internal error');
     }
 
 })
 
-app.post('/vagas', async (req,res) => {
+app.put('/vagas/:id_vaga', async (req, res) => {
+    try {
+        if (!req.body) return res.status(403).send('Para alterar um usuário, é necessário passar algum valor');
+
+        let index = await jobs.findIndex(job => job.id === req.params.id_vaga);
+        if (index >= 0) {
+            Object.keys(req.body).forEach(vaga => {
+                vagas[index][vaga] = req.body[vaga];
+            })
+            return res.send(`Vaga com o id ${req.params.id} alterada com sucesso`);
+        }
+        return res.status(403).send('Deu errado o ID');
+    } catch (error){
+        res.status(403).send('Internal Error');
+    }
+
+    //vagas[vagas.findIndex(element => element.id == req.params.id_vaga)] = req.body;
+})
+
+app.post('/vagas', async (req, res) => {
     try {
         let vagasLength = vagas.length;
         console.log(vagas.length);
         let vaga = createVaga(req.body);
         console.log('teste2');
         vagas.push(vaga);
-        if(vagas.length > vagasLength) return res.send('Added');
+        if (vagas.length > vagasLength) return res.send('Added');
         return res.status(500).send('Internal error');
     } catch (error) {
         return res.status(500).send('Internal error');
@@ -60,5 +80,3 @@ app.post('/vagas', async (req,res) => {
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
 })
-
-
